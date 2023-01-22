8211a036fd2cf776209b10a9904ae462ba3ba9a9
Patrick Bucher <patrick.bucher@mailbox.org>
2023-01-22 18:35:43 +0100

    allow setting min/max values

--- src/RandomNumberGenerator.elm
+++ src/RandomNumberGenerator.elm
@@ -3,7 +3,7 @@ module RandomNumberGenerator exposing (main)
 import Browser
 import Html exposing (Html, button, div, h1, input, label, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import Random
 -- elm install elm/random
 
@@ -16,6 +16,8 @@ type alias Model =
 type Msg
     = GenerateRandomNumber Int Int
     | GotRandomNumber Int
+    | ChangeMin String
+    | ChangeMax String
 
 randomNumberGenerator : Int -> Int -> Random.Generator Int
 randomNumberGenerator min max =
@@ -26,12 +28,20 @@ view model =
     div []
     [ h1 [] [ text "Random Number Generator" ]
     , label [] [ text "min." ]
-    , input [ type_ "number", value (String.fromInt model.min) ] []
+    , input [ type_ "number", value (String.fromInt model.min), onInput ChangeMin ] []
     , label [] [ text "max." ]
-    , input [ type_ "number", value (String.fromInt model.max) ] []
+    , input [ type_ "number", value (String.fromInt model.max), onInput ChangeMax ] []
     , button [ onClick (GenerateRandomNumber model.min model.max) ] [ text "Generate" ]
     , label [] [ text (String.fromInt model.randomNumber) ]
     ]
+
+parseInt : String -> Int -> Int
+parseInt str default =
+    case String.toInt str of
+        Just val ->
+            val
+        Nothing ->
+            default
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -40,6 +50,10 @@ update msg model =
             ( model, Random.generate GotRandomNumber (randomNumberGenerator min max) )
         GotRandomNumber randomNumber ->
             ( { model | randomNumber = randomNumber }, Cmd.none )
+        ChangeMin input ->
+            ( { model | min = parseInt input initialModel.min }, Cmd.none )
+        ChangeMax input ->
+            ( { model | max = parseInt input initialModel.max }, Cmd.none )
 
 initialModel : Model
 initialModel =
